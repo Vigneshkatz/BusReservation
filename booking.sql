8401ca87788e71d78fabbf2b49e66f48e4d32445
vignesh <vignesh000129@gmail.com>
2023-02-12 08:59:56 +0530

    bookingNumber column is added

--- booking.sql
+++ booking.sql
@@ -5,12 +5,6 @@ DROP TABLE BOOKING;
 		id INT PRIMARY KEY auto_increment,
 		busNumber INT NOT NULL ,
 		name VARCHAR(50) NOT NULL,
-		date DATE    
+		date DATE ,
+        bookingNumber VARCHAR(50)
 	);
-
-	INSERT INTO booking (busNumber,name,date)
-	VALUES (1,"Vignesh",'2022-01-15'),
-		(1,"KATZ",'2022-01-15'),
-		(3,"Vignesh",'2022-01-15');
-
-	SELECT * FROM booking;
